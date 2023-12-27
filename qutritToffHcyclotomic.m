2909103d7b8f3b0ac3ac67cd7efeb23b9b857305
Lia Yeh <lia@fullstackquantumcomputation.tech>
2023-12-27 00:43:53 -0800

    changed X in gate set to R1

--- qutritToffHcyclotomic.m
+++ qutritToffHcyclotomic.m
@@ -1,26 +1,27 @@
 %% Implementation of qutrit Toffoli+Hadamard gate set level operators
 % In paper: Exact synthesis of multiqutrit Clifford-cyclotomic circuits
 % For each level operator (-1)_[a], \omega_[a], X_[a,b], H_[a,b,c]: Add
-% controls or conjugate by classical reversible gates these constructions
-% for exact implementation on any number of qutrits with 2 borrowed ancilla
+% controls or conjugate by classical reversible gates constructions here
+% for exact implementation on any number of qutrits + 2 borrowed ancilla
 
 clear; % clear all variables so only the minimal gate set is in scope
-global d I II X H CCX; % declare the minimal gate set
+global d I II R1 H CCX; % declare the minimal gate set
 
 d = 3;
 I = eye(d);
 II = kron(I,I);
-X = init_X();
+R1 = init_R1();
 H = init_H();
 CCX = init_CCX();
 
 % Step through each line below to check its truth table (for gates that
 % are a diagonal gate times a d-ary classical reversible gate) or matrix
-truthU(X);
+truthU(R1);
 disp(H);
 truthU(CCX);
-truthU(Xdag());
 disp(Hdag());
+truthU(X());
+truthU(Xdag());
 truthU(Hsqm());
 truthU(wgp());
 truthU(CX());
@@ -47,9 +48,9 @@ end
 
 %% All functions here use only functions above it or in the same folder.
 
-function X = init_X()
-    global I;
-    X = I(:,[2:end,1]);
+function R1 = init_R1()
+    global d;
+    R1 = diag(arrayfun(@(k) exp(1i*2*pi/d)^k, 0:d-1));
 end
 
 function H = init_H()
@@ -62,22 +63,28 @@ function H = init_H()
 end
 
 function CCX = init_CCX()
-    global d X;
+    global d I;
+    X = I(:,[2:end,1]);
     CX = cellfun(@(k) X^k,num2cell([0:d-1]),'UniformOutput',false);
     CX = blkdiag(CX{:});
     CCX = cellfun(@(k) CX^k,num2cell([0:d-1]),'UniformOutput',false);
     CCX = blkdiag(CCX{:});
 end
 
-function Xdag = Xdag()
-    global d X;
-    Xdag = X^(d-1);
-end
-
 % Hdag with exact global phase. Is H^3 up to a global phase.
 function Hdag = Hdag()
     global d H;
     Hdag = H^(4*d-1);
+end
+
+function X = X()
+    global R1 H;
+    X = Hdag() * R1 * H;
+end
+
+function Xdag = Xdag()
+    global d;
+    Xdag = X()^(d-1);
 end
 
 % Sends |x> to -|-x mod d>. Is H^2 up to a global phase.
@@ -93,8 +100,8 @@ function wgp = wgp()
 end
 
 function CX = CX()
-    global d II X H CCX;
-    CX = (kron(X * Hsqm() * Xdag(), II) * CCX^((d+1)/2))^2;
+    global d II H CCX;
+    CX = (kron(X() * Hsqm() * Xdag(), II) * CCX^((d+1)/2))^2;
     CX = removeBorrowedAncilla(CX,1);
 end
 
@@ -114,11 +121,11 @@ end
 
 % |0>-controlled X gate: Does to target X if control is |0>, else does I
 function ZCX = ZCX()
-    global d I X H CCX;
+    global d I H CCX;
     nZCX = kron(kron(I,Hdag()^2),I) * CCX * kron(kron(I,H^2),I) * ...
         kron(CXdag(),I) * CCX * kron(CX(),I); % |not 0>-controlled X
     nZCX = removeBorrowedAncilla(nZCX,2);
-    ZCX = kron(I,X) * nZCX^(d-1);
+    ZCX = kron(I,X()) * nZCX^(d-1);
 end
 
 % |0>-controlled Xdag: Does to target Xdag if control is |0>, else I
@@ -129,8 +136,8 @@ end
 
 % The 1-qutrit S gate. Is the \omega_[2] operator applying \omega to |2>
 function S = S()
-    global X H;
-    S = (kron(X^2,H)*ZCX()*kron(X,H^3*X^2*H^2*X))^2;
+    global H;
+    S = (kron(X()^2,H)*ZCX()*kron(X(),H^3*X()^2*H^2*X()))^2;
     S = removeBorrowedAncilla(S,2);
 end
 
@@ -148,25 +155,25 @@ end
 
 % X_[0,1] level operator which is a 1-qutrit gate, for d = 3.
 function X0_1 = X0_1()
-    X0_1 = X^2*Hsqm()*X;
+    X0_1 = mgp()*X()^2*Hsqm()*X();
 end
 
 % X_[00,01] level operator, i.e. |0>-controlled X_[0,1] gate, for d = 3
 % which is unitary.  For d > 3, known constructions have computational
 % basis initialization and deterministic measurement due to X_[0,1].
 function X00_01 = X00_01()
-    global d I X H;
+    global d I H;
     udZCX = reIndex(ZCX(),[2 1]); % upside-down ZCX gate
     udZCXdag = reIndex(ZCXdag(),[2 1]);
     perm00_10_01 = udZCX * ZCX() * udZCXdag * ZCXdag();
     udCX = reIndex(CX(),[2 1]);
     udCXdag = reIndex(CXdag(),[2 1]);
-    X00_01 = kron(mgp()*X,X0_1()) * (perm00_10_01*udCX*perm00_10_01* ...
-        udCXdag*kron(X^2,I))^((d-1)/2);
+    X00_01 = kron(X(),X0_1()) * (perm00_10_01 * udCX * ...
+        perm00_10_01 * udCXdag * kron(X()^2,I))^((d-1)/2);
 end
 
 function X000_001 = X000_001()
-    global d I X;
+    global d I;
     CX0_1 = (kron(Xdag()^2,I) * X00_01)^((d-1)/2) * kron(Xdag(),I);
     X000_001 = kron(I,CX0_1) * kron(ZCX(),I) * kron(I,CX0_1) * ...
         kron(ZCXdag(),I) * reIndex(X00_01(),[1 3 2]);
@@ -175,9 +182,10 @@ end
 % The \omega_[22] level operator which applies phase \omega to |22>.
 % Is the |2>-controlled S gate.
 function w22 = w22()
-    global d II X H;
-    ZZCX = kron(II,X)*(kron(II,X)*X000_001())^(d-1); % |00>-controlled X
-    w22 = (kron(kron(X^2,X^2),H)*ZZCX*kron(kron(X,X),H^3*X^2*H^2*X))^2;
+    global d II H;
+    ZZCX = kron(II,X())*(kron(II,X())*X000_001())^(d-1); % |00>-ctrled X
+    w22 = ( kron(kron(X()^2,X()^2),H) * ZZCX * ...
+        kron(kron(X(),X()),H^3*X()^2*H^2*X()) )^2;
     w22 = removeBorrowedAncilla(w22,3);
 end
 
@@ -190,8 +198,7 @@ end
 
 % The (-1)_[2] level operator
 function M2 = M2()
-    global X;
-    M2 = kron(Xdag(),X)*X00_01()*kron(X,Xdag()) * Hdag2mwsq()^2;
+    M2 = kron(Xdag(),X())*X00_01()*kron(X(),Xdag()) * Hdag2mwsq()^2;
     M2 = removeBorrowedAncilla(M2,2);
 end
 
